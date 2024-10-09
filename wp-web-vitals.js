7516fda41024cdd4fcd950d2687c896afa4ca569
Gabor Angyal <gabor.angyal@codesharp.hu>
2024-10-09 10:26:07 +0200

    add measurements

--- wp-web-vitals.js
+++ wp-web-vitals.js
@@ -1,9 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (window.performance) {
-        var ttfb = performance.timing.responseStart - performance.timing.requestStart;
+        let measurements = {
+            ttfb: 0,
+            fcp: 0,
+            lcp: 0,
+            inp: 0,
+            cls: 0,
+            measurementSeconds: 0
+        };
+        const startTime = performance.now();
+        if (performance.getEntriesByType('navigation').length > 0) {
+            const navigationEntry = performance.getEntriesByType('navigation')[0];
+            measurements.ttfb = navigationEntry.responseStart - navigationEntry.requestStart;
+            console.log('TTFB:', measurements.ttfb);
+        }
+
+        function setMeasurementTime() {
+            measurements.measurementSeconds = (performance.now() - startTime) / 1000;
+        }
+
+        const observer = new PerformanceObserver((list) => {
+            for (const entry of list.getEntries()) {
+                if (entry.name === 'first-contentful-paint') {
+                    measurements.fcp = entry.startTime;
+                    setMeasurementTime();
+                    console.log('FCP:', entry.startTime);
+                } else if (entry.entryType === 'largest-contentful-paint') {
+                    measurements.lcp = entry.startTime;
+                    setMeasurementTime();
+                    console.log('LCP:', entry.startTime);
+                } else if (entry.entryType === 'event' && entry.name === 'interaction-to-next-paint') {
+                    measurements.inp = entry.duration;
+                    setMeasurementTime();
+                    console.log('INP:', entry.startTime);
+                } else if (entry.entryType === 'layout-shift' && !entry.hadRecentInput) {
+                    measurements.cls += entry.value;
+                    setMeasurementTime();
+                    console.log('CLS:', measurements.cls);
+                }
+            }
+        });
+
+        observer.observe({ type: 'paint', buffered: true });
+        observer.observe({ type: 'largest-contentful-paint', buffered: true });
+        observer.observe({ type: 'event', buffered: true, durationThreshold: 0 });
+        observer.observe({ type: 'layout-shift', buffered: true });
+
         var currentUrl = window.location.href;
 
-        // Get user type from WordPress
         var userType = 'guest';
         if (typeof wp !== 'undefined' && typeof wp.userSettings !== 'undefined') {
             console.log(wp.userSettings);
@@ -11,27 +55,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
 
-        // Send TTFB and URL to the server via AJAX
-        fetch(ttfbLogger.ajaxUrl, {
+        fetch(wpWebVitals.ajaxUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: new URLSearchParams({
-                action: 'log_ttfb',
-                ttfb: ttfb,
+                ...measurements,
+                action: 'log_webvitals',
                 userType: userType,
                 url: currentUrl,
-                nonce: ttfbLogger.nonce
+                nonce: wpWebVitals.nonce
             })
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log('TTFB and URL logged successfully:', data);
-            } else {
-                console.error('Error logging data:', data);
-            }
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log('TTFB and URL logged successfully:', data);
+                } else {
+                    console.error('Error logging data:', data);
+                }
         })
         .catch(error => {
             console.error('AJAX error:', error);
--- wp-web-vitals.php
+++ wp-web-vitals.php
@@ -22,6 +22,11 @@ function wp_web_vitals_create_table() {
     $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         ttfb float NOT NULL,
+        fcp: float NOT NULL,
+        lcp: float NOT NULL,
+        inp: float NOT NULL,
+        cls: float NOT NULL,
+        measurement_seconds: float NOT NULL,
         user_type varchar(255) DEFAULT '' NOT NULL,
         url text NOT NULL,
         user_agent text NOT NULL,
@@ -43,13 +48,18 @@ function wp_web_vitals_enqueue_script() {
     ]);
 }
 
-add_action('wp_ajax_nopriv_log_ttfb', 'wp_web_vitals_log_ttfb');
-add_action('wp_ajax_log_ttfb', 'wp_web_vitals_log_ttfb');
+add_action('wp_ajax_nopriv_log_webvitals', 'wp_web_vitals_log_webvitals');
+add_action('wp_ajax_log_webvitals', 'wp_web_vitals_log_webvitals');
 
-function wp_web_vitals_log_ttfb() {
+function wp_web_vitals_log_webvitals() {
     check_ajax_referer('wp-web-vitals-nonce', 'nonce');
 
     $ttfb = isset($_POST['ttfb']) ? floatval($_POST['ttfb']) : null;
+    $fcp = isset($_POST['fcp']) ? floatval($_POST['fcp']) : null;
+    $lcp = isset($_POST['lcp']) ? floatval($_POST['lcp']) : null;
+    $inp = isset($_POST['inp']) ? floatval($_POST['inp']) : null;
+    $cls = isset($_POST['cls']) ? floatval($_POST['cls']) : null;
+    $measurement_seconds = isset($_POST['measurementSeconds']) ? floatval($_POST['measurementSeconds']) : null;
     $user_type = isset($_POST['userType']) ? sanitize_text_field($_POST['userType']) : '';
     $url = isset($_POST['url']) ? sanitize_text_field($_POST['url']) : '';
 
@@ -63,6 +73,11 @@ function wp_web_vitals_log_ttfb() {
     $table_name = $wpdb->prefix . 'wp_web_vitals_logs';
     $wpdb->insert($table_name, [
         'ttfb' => $ttfb,
+        'fcp' => $fcp,
+        'lcp' => $lcp,
+        'inp' => $inp,
+        'cls' => $cls,
+        'measurement_seconds' => $measurement_seconds,
         'url' => $url,
         'user_type' => $user_type,
         'user_agent' => $user_agent,
